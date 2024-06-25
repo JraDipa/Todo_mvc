7538796a24af825a7e37862aefd6f1ae85033e87
Narotama <163377352+JraDipa@users.noreply.github.com>
2024-06-26 06:35:56 +0800

    Membuat koneksi database

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php 
+    class db{
+        private $host = "localhost";
+        private $user = 'dipa';
+        private $pass = '12345678';
+        private $db_name = 'todo_db';
+
+        public function connect(){
+            $con = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
+            $pdo = new PDO($con, $this->user, $this->pass);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        }
+    }
+?>
