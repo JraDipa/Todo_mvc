5e6f3ac606d4827a3bbdffc00674484c712bf61e
Narotama <163377352+JraDipa@users.noreply.github.com>
2024-06-26 07:10:58 +0800

    Menambah API untuk proses update dan delete

--- api/api.php
+++ api/api.php
@@ -18,6 +18,18 @@ if (isset($_GET['action'])) {
             $result = $todoController->add_todo($todo, $todo_data);
             echo json_encode(['message' => 'Todo added successfully']);
             break;
+        case 'update':
+            $id = $_GET['id'];
+            $topic = $_GET['topic'];
+            $todo = $_GET['todo'];
+            $result = $todoController->update_todo($id, $topic, $todo);
+            echo json_encode(['message' => 'Todo updated successfully']);
+            break;
+        case 'delete':
+            $id = $_GET['id'];
+            $result = $todoController->delete_todo($id);
+            echo json_encode(['message' => 'Todo deleted successfully']);
+            break;
     }
 } else {
     echo json_encode(['error' => 'Invalid action']);
